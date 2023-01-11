c5c786a0e01b34e2e1962901970e5c525e6294fe
OntoTube <35110747+KurumiFT@users.noreply.github.com>
2023-01-11 15:43:08 +0300

    attached work controller to Collector Work

--- src/ServerScriptService/CollectorController.server.lua
+++ src/ServerScriptService/CollectorController.server.lua
@@ -3,11 +3,26 @@ local Players = game:GetService('Players')
 
 local Dependencies = require(ReplicatedStorage.dependencies)
 local CollectorModelDependency = Dependencies.get('Collector')
+local CollectorModelFolder: Folder = Dependencies.getFolder('Collector')
+
+local Bindables_Folder: Folder = CollectorModelFolder.Bindables
+local Work_Event: BindableEvent = Bindables_Folder.Work
 
 local Models = {} -- Remembered models
 
-Players.PlayerAdded:Connect(function(player)
-    wait(5) -- For debug
+Work_Event.Event:Connect(function(player, state) -- Listen work model change event
+    if not state then
+        if not Models[player] then return end
+        Models[player]:Destroy()
+        Models[player] = nil
+    else
+        if Models[player] then return end
+        Models[player] = CollectorModelDependency.new(player) -- Create model for this player
+    end
+end)
 
-    local _Model = CollectorModelDependency.new(player)
+-- Debug part
+Players.PlayerAdded:Connect(function(player)
+    wait(10) -- For debug
+    game.ReplicatedStorage.WorkModel.Bindables.Hire:Fire(player, false, 'Collector')
 end)
